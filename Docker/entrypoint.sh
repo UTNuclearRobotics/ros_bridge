6bcab83ddc4f1d1175e7f93932e54d72d12d60b3
Nick Chan <nick.chan@utexas.edu>
2024-02-29 18:42:18 -0600

    initial commit

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 set -e
+echo "ROS1 bridge entrypoint"
 
-source /opt/ros/noetic/setup.bash
-# execute a command in the container
-roscore
+# source ROS2 and bridge environments
+source "/opt/ros/$ROS2_DISTRO/setup.bash"
+cd /bridge_ws
+source install/setup.bash
+
+# display bridged messages and launch the bridge
+ros2 run ros1_bridge dynamic_bridge --print-pairs
+ros2 run ros1_bridge dynamic_bridge
+exec "$@"
